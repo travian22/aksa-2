b68021ec888ecbc9ef4429b6411d3951df497748
Septaro Travian Gadha <septaro21@gmail.com>
2026-02-10 14:48:44 +0700

    fix: remove Faker dependency from UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,11 +23,9 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        $faker = \Faker\Factory::create();
-
         return [
-            'name' => $faker->name(),
-            'email' => $faker->unique()->safeEmail(),
+            'name' => 'User ' . Str::random(5),
+            'email' => Str::random(8) . '@example.com',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
